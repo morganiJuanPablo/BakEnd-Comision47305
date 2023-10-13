b481a6a46c2ad83633e481d2337125d126008f68
unknown <morgani.jpg@gmail.com>
2023-10-13 21:00:59 +0200

    últimos detalles

--- E-Comerce-Discos-Vinilo/src/public/js/cart.js
+++ E-Comerce-Discos-Vinilo/src/public/js/cart.js
@@ -11,3 +11,26 @@ const deleteProductCart = async (productId) => {
     console.error("Error de red:", error);
   }
 };
+
+const emptyCart = async () => {
+  try {
+    const response = await fetch(`/cart/65227efc9ddfa566df832c8e`, {
+      method: "DELETE",
+    });
+    const result = await response.json();
+    location.reload();
+  } catch (error) {
+    console.error("Error de red:", error);
+  }
+};
+
+async function emptyCartCheck() {
+  const cartContainerCards = await document.getElementById(
+    "cartContainerCards"
+  );
+  if (!cartContainerCards.innerHTML.trim()) {
+    cartContainerCards.innerHTML = `<p class='emptyCartmessage'> El carrito está vacío<br>:(</p>`
+  }
+}
+
+emptyCartCheck();
